9abbb0e4ef2b502c180583cbf7cbdae4180e2705
mgroth0 <mgroth49@gmail.com>
2022-09-10 15:06:26 -0400

    autocommit

--- src/commonMain/kotlin/matt/collect/weak/weak.kt
+++ src/commonMain/kotlin/matt/collect/weak/weak.kt
@@ -1,5 +1,9 @@
 package matt.collect.weak
 
+import matt.collect.dmap.withStoringDefault
+
+fun <K, V> lazyWeakMap(op: (K)->V) = WeakMap<K, V>().withStoringDefault { op(it) }
+
 /*SERIOUS EQUALITY ISSUES HAVE LEAD ME TO USING HASH CODES INSTEAD OF THE OBJECTS THEMSELVES*/
 /*... IT WORKED*/
-expect class WeakMap<K, V>: MutableMap<K, V>
+expect class WeakMap<K, V>(): MutableMap<K, V>
