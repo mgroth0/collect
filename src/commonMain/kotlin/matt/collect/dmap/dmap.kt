b81965abf29b0b2639d59380b35079e969e73b4f
mgroth0 <mgroth49@gmail.com>
2022-10-04 18:06:36 -0400

    autocommit

--- src/commonMain/kotlin/matt/collect/dmap/dmap.kt
+++ src/commonMain/kotlin/matt/collect/dmap/dmap.kt
@@ -12,43 +12,40 @@ interface CanBeNotNullMap<K, V>: Map<K, V> {
   override operator fun get(key: K): V
 }
 
-class DefaultStoringMap<K, V>(
-  val map: MutableMap<K, V>,
-  val d: (K)->V
+
+expect class DefaultStoringMap<K, V>(
+  map: MutableMap<K, V>,
+  d: (K)->V
 ): CanBeNotNullMutableMap<K, V> {
+  internal val map: MutableMap<K, V>
+  internal val d: (K)->V
+
   override val size: Int
-	get() = map.size
 
-  override fun containsKey(key: K) = map.containsKey(key)
+  override fun containsKey(key: K): Boolean
 
-  override fun containsValue(value: V) = map.containsValue(value)
+  override fun containsValue(value: V): Boolean
 
-  override operator fun get(key: K): V {
-	return map[key] ?: d(key).also {
-	  map[key] = it
-	}
-  }
 
-  fun getWithoutSetting(key: K): V? {
-	return map[key]
-  }
+  override operator fun get(key: K): V
 
-  override fun isEmpty() = map.isEmpty()
+  fun getWithoutSetting(key: K): V?
+
+  override fun isEmpty(): Boolean
 
   override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
-	get() = map.entries
   override val keys: MutableSet<K>
-	get() = map.keys
   override val values: MutableCollection<V>
-	get() = map.values
 
-  override fun clear() = map.clear()
+  override fun clear()
 
-  override fun put(key: K, value: V): V? = map.put(key, value)
+  override fun put(key: K, value: V): V?
 
-  override fun putAll(from: Map<out K, V>) = map.putAll(from)
+  override fun putAll(from: Map<out K, V>)
 
-  override fun remove(key: K): V? = map.remove(key)
+  override fun remove(key: K): V?
 
 }
+
+class SimpleObjWrapper<T>(val o: T)
 
---
+++ src/jvmMain/kotlin/matt/collect/dmap/dmap.kt
@@ -0,0 +1,92 @@
+package matt.collect.dmap
+
+import java.util.concurrent.Semaphore
+import kotlin.contracts.InvocationKind.EXACTLY_ONCE
+import kotlin.contracts.contract
+
+actual class DefaultStoringMap<K, V> actual constructor(
+  actual val map: MutableMap<K, V>,
+  actual val d: (K)->V
+): CanBeNotNullMutableMap<K, V> {
+  actual override val size: Int
+	get() = map.size
+
+  actual override fun containsKey(key: K) = map.containsKey(key)
+
+  actual override fun containsValue(value: V) = map.containsValue(value)
+
+  private val multiMonitor = MultiMonitor<K>()
+
+  actual override operator fun get(key: K): V {
+	return multiMonitor.with(key) {
+	  map[key] ?: d(key).also {
+		map[key] = it
+	  }
+	}
+  }
+
+  actual fun getWithoutSetting(key: K): V? {
+	return map[key]
+  }
+
+  actual override fun isEmpty() = map.isEmpty()
+
+  actual override val entries: MutableSet<MutableMap.MutableEntry<K, V>>
+	get() = map.entries
+  actual override val keys: MutableSet<K>
+	get() = map.keys
+  actual override val values: MutableCollection<V>
+	get() = map.values
+
+  actual override fun clear() = map.clear()
+
+  actual override fun put(key: K, value: V): V? = map.put(key, value)
+
+  actual override fun putAll(from: Map<out K, V>) = map.putAll(from)
+
+  actual override fun remove(key: K): V? = map.remove(key)
+
+}
+
+
+class MultiMonitor<T>() {
+
+
+  fun <R> with(t: T, op: ()->R): R {
+	contract {
+	  callsInPlace(op, EXACTLY_ONCE)
+	}
+	val sem = takeMonitor(t)
+	sem.acquire()
+	val r = op()
+	sem.release()
+	giveBackMonitor(t)
+	return r
+  }
+
+
+  private val currentlyHeld = mutableMapOf<T, Semaphore>()
+  private val currentlyWaiting = mutableMapOf<T, Int>()
+
+  @Synchronized
+  private fun takeMonitor(t: T): Semaphore {
+	val r = currentlyHeld[t] ?: Semaphore(1).also {
+	  currentlyHeld[t] = it
+	}
+	val c = currentlyWaiting[t] ?: 0
+	currentlyWaiting[t] = c + 1
+	return r
+  }
+
+  @Synchronized
+  private fun giveBackMonitor(t: T) {
+	val c = currentlyWaiting[t]!!
+	val newC = c - 1
+	currentlyWaiting[t] = newC
+	if (newC == 0) {
+	  currentlyWaiting.remove(t)
+	  currentlyHeld.remove(t)
+	}
+  }
+
+}
