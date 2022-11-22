7edf5fb5943243f288ba8618e4bb0187a7fbf392
mgroth0 <mgroth49@gmail.com>
2022-11-21 19:02:54 -0500

    autocommit

--- src/commonMain/kotlin/matt/collect/list/list.kt
+++ src/commonMain/kotlin/matt/collect/list/list.kt
@@ -2,3 +2,10 @@ package matt.collect.list
 
 fun <E> List<E>.readOnly() = ReadOnlyList(this)
 class ReadOnlyList<E>(private val list: List<E>): List<E> by list
+
+
+fun <E> List<E>.phase(newStartIndex: Int) = (newStartIndex until newStartIndex.size).map {
+  this[it]
+} + (0 until newStartIndex).map {
+  this[it]
+}
