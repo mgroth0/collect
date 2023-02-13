de8dce73940b0f7a0622b8165f54e34b95fb960f
mgroth0 <mgroth49@gmail.com>
2023-02-13 17:48:18 -0500

    autocommit

--- src/commonMain/kotlin/matt/collect/fake/fake.kt
+++ src/commonMain/kotlin/matt/collect/fake/fake.kt
@@ -1,23 +1,24 @@
 package matt.collect.fake
 
 import matt.collect.itr.toFakeMutableIterator
-import matt.lang.ILLEGAL
+import matt.lang.err
 
 fun <E> List<E>.toFakeMutableList() = FakeMutableList(this)
 
 class FakeMutableList<E>(private val list: List<E>): List<E> by list, MutableList<E> {
-  override fun add(element: E) = ILLEGAL
-  override fun add(index: Int, element: E) = ILLEGAL
-  override fun addAll(index: Int, elements: Collection<E>) = ILLEGAL
-  override fun addAll(elements: Collection<E>) = ILLEGAL
-  override fun clear() = ILLEGAL
-  override fun removeAt(index: Int) = ILLEGAL
-  override fun set(index: Int, element: E) = ILLEGAL
-  override fun retainAll(elements: Collection<E>) = ILLEGAL
-  override fun removeAll(elements: Collection<E>) = ILLEGAL
-  override fun remove(element: E) = ILLEGAL
+  private fun theErr(): Nothing = err("$this is not really mutable")
+  override fun add(element: E) = theErr()
+  override fun add(index: Int, element: E) = theErr()
+  override fun addAll(index: Int, elements: Collection<E>) = theErr()
+  override fun addAll(elements: Collection<E>) = theErr()
+  override fun clear() = theErr()
+  override fun removeAt(index: Int) = theErr()
+  override fun set(index: Int, element: E) = theErr()
+  override fun retainAll(elements: Collection<E>) = theErr()
+  override fun removeAll(elements: Collection<E>) = theErr()
+  override fun remove(element: E) = theErr()
   override fun iterator() = list.iterator().toFakeMutableIterator()
-  override fun listIterator() = ILLEGAL
-  override fun listIterator(index: Int) = ILLEGAL
-  override fun subList(fromIndex: Int, toIndex: Int) = ILLEGAL
+  override fun listIterator() = theErr()
+  override fun listIterator(index: Int) = theErr()
+  override fun subList(fromIndex: Int, toIndex: Int) = theErr()
 }
