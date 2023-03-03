dbaef8dec13fb988c5f32fb936e3df5afc3af780
mgroth0 <mgroth49@gmail.com>
2023-03-03 12:35:13 -0500

    autocommit

---
+++ src/commonMain/kotlin/matt/collect/list/sub/sub.kt
@@ -0,0 +1,48 @@
+package matt.collect.list.sub
+
+class SubList<E>(
+
+): List<E> {
+  override val size: Int
+	get() = TODO("Not yet implemented")
+
+  override fun contains(element: E): Boolean {
+	TODO("Not yet implemented")
+  }
+
+  override fun containsAll(elements: Collection<E>): Boolean {
+	TODO("Not yet implemented")
+  }
+
+  override fun get(index: Int): E {
+	TODO("Not yet implemented")
+  }
+
+  override fun indexOf(element: E): Int {
+	TODO("Not yet implemented")
+  }
+
+  override fun isEmpty(): Boolean {
+	TODO("Not yet implemented")
+  }
+
+  override fun iterator(): Iterator<E> {
+	TODO("Not yet implemented")
+  }
+
+  override fun lastIndexOf(element: E): Int {
+	TODO("Not yet implemented")
+  }
+
+  override fun listIterator(): ListIterator<E> {
+	TODO("Not yet implemented")
+  }
+
+  override fun listIterator(index: Int): ListIterator<E> {
+	TODO("Not yet implemented")
+  }
+
+  override fun subList(fromIndex: Int, toIndex: Int): List<E> {
+	TODO("Not yet implemented")
+  }
+}
