88c37b03bebfb81d493f56a54057fcf4980d8a3e
mgroth0 <mgroth49@gmail.com>
2023-06-14 00:53:50 -0400

    changed src/commonMain/kotlin/matt/collect/fake/fake.kt

--- src/commonMain/kotlin/matt/collect/fake/fake.kt
+++ src/commonMain/kotlin/matt/collect/fake/fake.kt
@@ -23,3 +23,49 @@ class FakeMutableList<E>(private val list: List<E>): List<E> by list, MutableLis
   override fun listIterator(index: Int) = list.listIterator(index).toFakeMutableListIterator()
   override fun subList(fromIndex: Int, toIndex: Int) = theErr()
 }
+
+
+class FakeList<E>: List<E> {
+  override val size: Int
+    get() = error("this is fake")
+
+  override fun contains(element: E): Boolean {
+    error("this is fake")
+  }
+
+  override fun containsAll(elements: Collection<E>): Boolean {
+    error("this is fake")
+  }
+
+  override fun get(index: Int): E {
+    error("this is fake")
+  }
+
+  override fun indexOf(element: E): Int {
+    error("this is fake")
+  }
+
+  override fun isEmpty(): Boolean {
+    error("this is fake")
+  }
+
+  override fun iterator(): Iterator<E> {
+    error("this is fake")
+  }
+
+  override fun lastIndexOf(element: E): Int {
+    error("this is fake")
+  }
+
+  override fun listIterator(): ListIterator<E> {
+    error("this is fake")
+  }
+
+  override fun listIterator(index: Int): ListIterator<E> {
+    error("this is fake")
+  }
+
+  override fun subList(fromIndex: Int, toIndex: Int): List<E> {
+    error("this is fake")
+  }
+}
